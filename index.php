a1e2be72149250033e76081e146ed133e328a827
Lauliek <laureline.pellegrini@gmail.com>
2024-11-21 09:52:16 +0100

    Les variables exercices 1 à 3 ensemble avec retour à la ligne - prochain commit catégorie les variables entière

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 /* PHP - Les variables  */
 
 /* Exercice 1 : Créer une variable nom et l'initialiser avec la valeur de votre choix. Afficher son contenu  */
-/* $nom = "Pierre";
 
-echo $nom;
- */
+$nom = "Pierre";
+
+echo $nom."<br>";
+
 
 /* Exercice 2 : Créer trois variables nom , prenom et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier.
 Afficher leur contenu.  */
@@ -15,4 +16,18 @@ $nom = "Paul";
 $prenom = "Jacques";
 $age = 77;
 
-echo $nom." ".$prenom." ".$age;
+echo $nom." ".$prenom." ".$age."<br>";
+
+
+/* Exercice 3 :
+Créer une variable km. L'initialiser à 1. Afficher son contenu.
+Changer sa valeur par 3. Afficher son contenu.
+Changer sa valeur par 125. Afficher son contenu. */
+
+$km = 1;
+echo $km."<br>";
+$km = 3;
+echo $km."<br>";
+$km = 125;
+echo $km."<br>";
+
