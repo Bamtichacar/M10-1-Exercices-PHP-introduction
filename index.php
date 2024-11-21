c6ccbc62fe47360f69aa5250f2abed578b404e37
Lauliek <laureline.pellegrini@gmail.com>
2024-11-21 09:45:05 +0100

    les variables exercice 2

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 /* PHP - Les variables  */
 
 /* Exercice 1 : Créer une variable nom et l'initialiser avec la valeur de votre choix. Afficher son contenu  */
-$nom = "Pierre";
+/* $nom = "Pierre";
 
 echo $nom;
+ */
 
+/* Exercice 2 : Créer trois variables nom , prenom et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier.
+Afficher leur contenu.  */
+
+$nom = "Paul";
+$prenom = "Jacques";
+$age = 77;
+
+echo $nom." ".$prenom." ".$age;
