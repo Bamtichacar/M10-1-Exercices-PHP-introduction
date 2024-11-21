f869560f4845c7b403ee1604ed59d75b0aae9dae
Lauliek <laureline.pellegrini@gmail.com>
2024-11-21 09:43:48 +0100

    les variables exercice 1

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "TEST";
-?>
+
+/* PHP - Les variables  */
+
+/* Exercice 1 : Créer une variable nom et l'initialiser avec la valeur de votre choix. Afficher son contenu  */
+$nom = "Pierre";
+
+echo $nom;
+
